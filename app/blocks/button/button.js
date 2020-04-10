637b2cdcfd8ad85738351a3fca2a5685dab7e5a1
Gleb Gorokhov <gg@verstaburo.com>
2020-04-10 16:11:34 +0300

    Update 2020-04-10T13:11:00.338Z

--- app/blocks/button/button.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const $ = window.$;
-
-export function button() {
-  $(document).on('click', '.Button_submit-button:not(.is-sent)', function () {
-    $(this).addClass('is-sent');
-  });
-}
-/* eslint-enable */
